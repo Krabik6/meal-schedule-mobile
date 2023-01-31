9f36806601e2591c039a8d72ad77a4152ea468bd
Lev <lev1237oo@gmail.com>
2023-01-31 20:51:12 +0400

    switch between Login and Signup activities fix bugs

--- app/src/main/java/com/example/schedule_app/MainActivity.kt
+++ app/src/main/java/com/example/schedule_app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.schedule_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var signupButton : Button = findViewById<Button>(R.id.SignupButton)
+        signupButton.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
+
--- app/src/main/java/com/example/schedule_app/SignUp.kt
+++ app/src/main/java/com/example/schedule_app/SignUp.kt
@@ -1,11 +1,18 @@
 package com.example.schedule_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        var loginButton : Button = findViewById<Button>(R.id.LoginButton)
+        loginButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
